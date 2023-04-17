cfdc236bb7bbd664da3835fec7d9022ccf8c47c7
hassankafiye <xasankaafiye1999@gmail.com>
2023-04-17 18:05:40 +0100

    react

---
+++ src/Component/Center.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Center() {
+  return (
+    <div className='Center'>
+      <h1>MY NAME: hassan maslax hussein</h1>
+      <h1>mY ID C120256</h1>
+    </div>
+  );
+}
+
+export default Center;
